5db1ca7a9ebebb5d0375d18e19e0d51fc9cd73bd
sirdan <stanford303@gmail.com>
2022-06-08 17:34:40 -0700

    made changes

--- src/BooksItem.js
+++ src/BooksItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
 export default function BooksItem({ books }) {
   return (
-    <div className='books-item'>
-      <h1>{books.name}</h1>
-      <p>{books.type}</p>
-      
+    <><div className='books-item'>
+      <h1>This Is The Author {books.name}</h1>
+      <p>These Type of Books Are {books.type}</p>
     </div>
+    <div className='book'>
+      {books.book.map((book) => (
+        <p key={book}>{book}</p>
+      ))}
+    </div></>
+      
   );
 }
                
--- src/books.js
+++ src/books.js
@@ -4,4 +4,14 @@ export const books = [
     type:'Fiction',
     book: ['Take Me To WanderLand', 'Silly Sally Sue And The Wondering Trolls', 'Kyle And His Friend Dragon']
   },
+  {
+    name:'Tim Slips',
+    type:'Fiction',
+    book: ['Magic Dwarf Land', 'Def Monkeys At War', 'Planet of the Bee people']
+  },
+  {
+    name:'Craig wheeler',
+    type:'Fiction',
+    book: ['Humpty Dumpty Loves Fried Eggs', 'Charlie And The Haunted Factory', 'The Boogie Man Part ||']
+  },
 ];
